47e4a645f2f1499f28984f8c623d4445c5744e0d
nicolegros <nicolaslegros98@gmail.com>
2023-10-25 08:26:10 -0400

    :sparkles: Data gathering for mirantis

--- src/repoextractor.py
+++ src/repoextractor.py
@@ -2,8 +2,10 @@ import json
 import os
 
 import pandas as pd
+import requests
+from codetiming import Timer
 from dotenv import load_dotenv
-from github import Github
+from github import Github, GithubException
 from github.Commit import Commit
 from github.PaginatedList import PaginatedList
 from github.Repository import Repository
@@ -16,11 +18,15 @@ class RepoExtractor:
         self.batch_size = batch_size
         load_dotenv()
         self.api = Github(os.getenv('GITHUB_TOKEN'))
+        self.timer = Timer()
 
     def _get_orgs_repos(self, name: str) -> PaginatedList[Repository]:
         repositories = self.api.search_repositories(query="user:{}".format(name))
         print(f"Organization '{name}' has {repositories.totalCount} repositories")
         return repositories
+
+    def get_repo(self, fullname: str) -> Repository:
+        return self.api.get_repo(fullname)
 
     def extract_repo_names(self, organization: str) -> list[Repository]:
         paginated_list = self._get_orgs_repos(organization)
@@ -35,22 +41,25 @@ class RepoExtractor:
         return repos
 
     def extract_repo_with_fullname(self, name: str) -> list[Commit]:
+        print(f"        {self.timer.stop}")
+        self.timer = Timer()
+        self.timer.start()
         repo = self.api.get_repo(name)
-        return self._extract_repo(repo, name.split("/")[0].lower())
+        return self.extract_repo(repo, name.split("/")[0].lower())
 
-    def _extract_repo(self, repo: Repository, org_name: str):
+    def extract_repo(self, repo: Repository, org_name: str):
         processed = {}
         print(f"    Extracting commits from repo '{repo.name}'")
         commits = []
-        for c in repo.get_commits():
+        get_commits = repo.get_commits()
+        print(f"    {get_commits.totalCount} commits total")
+        for c in get_commits:
             commits.append(c)
             processed[c.sha] = {
                 "files": list(map(lambda file: file.filename, c.files)),
-                "date": c.commit.committer.date,
-                "message": c.commit.message
             }
         df = pd.DataFrame.from_dict(processed, orient="index")
-        complete_save_path = f"{self.data_folder}/{org_name}/{repo.name}.commits.pickle"
+        complete_save_path = f"{self.data_folder}/{org_name}/commits-files/{repo.name}.commits.pickle"
         print(f"        Saving commits to {complete_save_path}")
         df.to_pickle(complete_save_path)
         return commits
@@ -61,9 +70,42 @@ class RepoExtractor:
         extracted_repos = []
         for repo in self._get_orgs_repos(organization_name):
             if repo.full_name not in repos_to_ignore:
-                extracted_repos.append(self._extract_repo(repo, organization_name))
+                extracted_repos.append(self.extract_repo(repo, organization_name))
         return repos_to_ignore
 
+    @staticmethod
+    def get_commits_url(repo_fullname: str, page: int):
+        return f"https://api.github.com/repos/{repo_fullname}/commits?per_page=100&page={page}"
 
-# ======= Run =======
-RepoExtractor().extract_repos_from_organization("mirantis")
+    def extract_raw_commits_from_repo(self, repo_fullname):
+        load_dotenv()
+        try:
+            total_commits = self.api.get_repo(repo_fullname).get_commits().totalCount
+        except GithubException:
+            print(f"{repo_fullname} is empty")
+            return []
+
+        commits = []
+        for i in range(int(total_commits / 100)):
+            cs = requests.get(self.get_commits_url(repo_fullname, i + 1),
+                              headers={"Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"}).json()
+            commits.extend(cs)
+
+        with open(f"../data/raw/{repo_fullname.lower()}.commits.json", "w") as file:
+            json.dump(commits, file)
+        return commits
+
+    def save_commits_messages_dates(self, raw_path: str, org: str, repo: str):
+        with open(f"{raw_path}") as file:
+            data = json.load(file)
+        ps = {}
+        for d in data:
+            processed = {
+                "message": d["commit"]["message"],
+                "date": d["commit"]["committer"]["date"]
+            }
+            ps[d["sha"]] = processed
+
+        df = pd.DataFrame.from_dict(ps, orient="index")
+        df.to_pickle(f'{self.data_folder}/{org}/commits-messages-dates/{repo}.pickle')
+        return df
--- src/repovalidator.py
+++ src/repovalidator.py
@@ -7,15 +7,17 @@ import pandas as pd
 
 class RepoValidator:
     def __init__(self, repo_pickle_file: str):
-        self.df = pd.read_pickle(repo_pickle_file)
+        self.df: pd.DataFrame = pd.read_pickle(repo_pickle_file)
 
     def is_valid(self) -> bool:
         return self.has_11_percent_of_iac() and self.has_at_least_2_commits_per_month()
 
     def has_at_least_2_commits_per_month(self) -> bool:
+        if self.df.size < 1:
+            return False
         self.df.loc[:, 'month'] = pd.to_datetime(self.df.date).dt.month
         self.df.loc[:, 'year'] = pd.to_datetime(self.df.date).dt.year
-        return (self.df.groupby(['year', 'month']).count() >= 2).all()["files"]
+        return (self.df.groupby(['year', 'month']).count() >= 2).all()["date"]
 
     def has_11_percent_of_iac(self) -> bool:
         list_folder = []
@@ -24,12 +26,18 @@ class RepoValidator:
         unique_files = np.unique(list_folder).tolist()
 
         is_iac_list = []
-        for folder in unique_files:
-            is_iac_list.append(self._is_iac(folder))
-        # print(unique_files)
+        for file in unique_files:
+            is_iac_list.append(self._is_iac(file))
         return (sum(is_iac_list) / len(is_iac_list)) >= 0.11
 
     @staticmethod
     def _is_iac(filename: str) -> bool:
-        keywords = [".pp", "docker", "travis", "infra", "puppet"]
-        return reduce(lambda before, keyword: keyword in filename.lower() and before, keywords, True)
+        keywords = [".pp",
+                    "docker",
+                    "travis",
+                    "infra",
+                    "puppet",
+                    "k8s",
+                    "environment",
+                    "kubernetes"]
+        return reduce(lambda before, keyword: keyword in filename.lower() or before, keywords, False)
