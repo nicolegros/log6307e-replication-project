c1a94114671d6b203fb54217fcf128519943e56a
thomtrep <thomtrepanier@gmail.com>
2023-10-27 10:24:28 -0400

    commit processor for OpenStack + Results

---
+++ src/gitea_commit_message_processor.py
@@ -0,0 +1,93 @@
+from functools import reduce
+import re
+
+import numpy as np
+import pandas as pd
+import json
+import giteapy
+
+import requests
+
+
+class GiteaCommitMessageProcessor:
+    def __init__(self, api_url: str, data_folder="../data/processed"):
+        self.data_folder = data_folder
+        self.issue_identifier_pattern = re.compile(r"#\d{1,10}")
+        self.api_url = api_url
+
+        self.configuration = giteapy.Configuration()
+        self.configuration.host = self.api_url
+        self.issue_api = giteapy.IssueApi(giteapy.ApiClient(self.configuration))
+
+    def extract_xcm(self, org: str, repo_name: str) -> list :
+        self.commits_df: pd.DataFrame = pd.read_pickle(f"{self.data_folder}/{org}/commits-files/{repo_name}.commits.pickle")
+        self.commit_messsage_df: pd.DataFrame = pd.read_pickle(f"{self.data_folder}/{org}/commits-messages-dates/{repo_name}.pickle")
+        print(f"    Extracting XCM from repo '{repo_name}'")
+        print(f"   {self.commits_df.size} commits total")
+
+        xcms: list[dict] = []
+        for sha in self.commits_df.index:
+            if (not self.__commit_modifies_iac(sha)):
+                continue
+
+            commit_message = self.__extract_commit_message(sha)
+            issue_identifier = self.__extract_issue_identifier(commit_message)
+            issue_summary = ""
+            if issue_identifier:
+                try:
+                    issue = self.issue_api.issue_get_issue(owner=org, repo = repo_name, index=issue_identifier)
+                except Exception as e:
+                    print(f"{e}")
+                    issue = None
+
+                # Assign issue_summary to issue["body"] if the dictionnary as the "body" key and blank string else
+                issue_summary = issue["body"] if issue and "body" in issue else ""
+            xcm = self.__convert_to_xcm(commit_message, issue_summary)
+            xcms.append(xcm)
+
+        # Save the xcms in json
+        complete_save_path = f"{self.data_folder}/{org}/xcms/{repo_name}.json"
+        print(f"        Saving xcms to {complete_save_path}")
+        with open(complete_save_path, "w") as f:
+            json.dump(xcms, f, indent=4)
+        
+        return xcms
+    
+    def __commit_modifies_iac(self, commit_sha: str) -> bool:
+        return reduce(lambda before, file: self._is_iac(file) or before, self.commits_df.loc[commit_sha].files, False)
+    
+    def __extract_commit_message(self, commit_sha: str) -> str: 
+        if commit_sha not in self.commit_messsage_df.index:
+            return ""
+        
+        return self.commit_messsage_df.loc[commit_sha].message
+    
+    def __extract_issue_identifier(self, commit_message: str) -> str | None:
+        issue_identifier = None
+        match = self.issue_identifier_pattern.search(commit_message)
+        if match:
+            issue_identifier = match.group(0)
+
+        return issue_identifier
+    
+    def __convert_to_xcm(self, commit_message: str, issue_summary: str) -> dict:
+        return {
+            "commit_message": commit_message,
+            "issue_summary": issue_summary
+        }
+
+    @staticmethod
+    def get_issue_url(owner: str, repo: str, issue_number: str) -> str:
+        return f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}"
+    
+    @staticmethod
+    def _is_iac(filename: str) -> bool:
+        keywords = [".pp",
+                    "docker",
+                    "travis",
+                    "infra",
+                    "puppet",
+                    "k8s",
+                    "environment",
+                    "kubernetes"]
+        return reduce(lambda before, keyword: keyword in filename.lower() or before, keywords, False)
